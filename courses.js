ab5b69f4f65cd587d7effb6e9caaf3274374712d
Aladdin Sonni <aladdin.sonni@gmail.com>
2022-07-24 15:22:01 +0200

    first commit

---
+++ courses.js
@@ -0,0 +1,11 @@
+// Put courses you want in this array
+
+module.exports = [
+  // 'https://www.vuemastery.com/courses/vue3-typescript/why-vue-&-typescript',
+  // 'https://www.vuemastery.com/courses/typescript-friendly-vue3/introduction-to-the-script-setup-syntax',
+  // 'https://www.vuemastery.com/courses/vue-3-essentials/setup-and-reactive-references'
+
+  // 'https://www.vuemastery.com/courses/validating-vue3-forms',
+
+  'https://www.vuemastery.com/courses/unit-testing-vue-3/what-to-test-vue-3/'
+];
